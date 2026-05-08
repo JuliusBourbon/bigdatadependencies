1647cf74e7e68ebd575b2cae7c254f72067310ce
JuliusBourbon <naherrrrr@gmail.com>
2026-05-08 14:52:26 +0700

    fix lower case formatting

--- mapper.py
+++ mapper.py
@@ -3,7 +3,7 @@ import sys
 import re
 
 for line in sys.stdin:
-    clean_line = re.sub(r'[.,;!?"\'-]', '', line).lower()
+    clean_line = re.sub(r'[.,;!?"\'-]', '', line)
     words = clean_line.strip().split()
     for word in words:
         print "%s\t1" % word
